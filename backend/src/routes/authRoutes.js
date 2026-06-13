6dc1203dd0f97bd8c29f0186d592eab7c317870e
Kamalesh-git23 <kamalesh23112005@gmail.com>
2026-06-13 23:14:41 +0530

    feat: implement authentication module

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,119 @@
+import express from "express";
+
+import {
+  register,
+  login,
+  getProfile,
+} from "../controllers/authController.js";
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+import {
+  registerSchema,
+  loginSchema,
+} from "../validations/authValidation.js";
+
+import { validate } from "../middleware/validate.js";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register User
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Kamalesh
+ *               email:
+ *                 type: string
+ *                 example: kamal@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       201:
+ *         description: User Registered Successfully
+ *       400:
+ *         description: User Already Exists
+ *       500:
+ *         description: Internal Server Error
+ */
+router.post(
+  "/register",
+  validate(registerSchema),
+  register
+);
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login User
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: kamal@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: Login Successful
+ *       401:
+ *         description: Invalid Credentials
+ *       500:
+ *         description: Internal Server Error
+ */
+router.post(
+  "/login",
+  validate(loginSchema),
+  login
+);
+
+/**
+ * @swagger
+ * /api/auth/profile:
+ *   get:
+ *     summary: Get User Profile
+ *     tags:
+ *       - Authentication
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User Profile
+ *       401:
+ *         description: Unauthorized
+ */
+router.get(
+  "/profile",
+  authMiddleware,
+  getProfile
+);
+
+export default router;
