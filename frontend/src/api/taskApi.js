bc964dd20b977a7569d827c7ef5a9ba3f9f96090
Kamalesh-git23 <kamalesh23112005@gmail.com>
2026-06-15 10:00:53 +0530

    feat: create frontend API integration layer

---
+++ frontend/src/api/taskApi.js
@@ -0,0 +1,25 @@
+import api from "./axios";
+
+export const getTasks = () =>
+  api.get("/tasks");
+
+export const getTaskById = (id) =>
+  api.get(`/tasks/${id}`);
+
+export const createTask = (data) =>
+  api.post("/tasks", data);
+
+export const updateTask = (id, data) =>
+  api.put(`/tasks/${id}`, data);
+
+export const deleteTask = (id) =>
+  api.delete(`/tasks/${id}`);
+
+export const updateTaskState = (
+  id,
+  state
+) =>
+  api.patch(
+    `/tasks/${id}/state`,
+    { state }
+  );
