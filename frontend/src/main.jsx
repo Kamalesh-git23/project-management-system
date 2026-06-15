eeabc51b74e431476d51311479e095d72a6f07d9
Kamalesh-git23 <kamalesh23112005@gmail.com>
2026-06-15 10:08:23 +0530

    feat: integrate frontend authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AuthProvider from './context/AuthContext.jsx'
 import ProjectProvider from './context/ProjectContext.jsx'
 import TaskProvider from './context/TaskContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ProjectProvider>
-    <TaskProvider>
-      <App />
-    </TaskProvider>
-  </ProjectProvider>
+  <AuthProvider>
+    <ProjectProvider>
+      <TaskProvider>
+        <App />
+      </TaskProvider>
+    </ProjectProvider>
+  </AuthProvider>
 )
